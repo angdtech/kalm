2e231abb1406d83f2c7cec7f3b1f49010e50ddfa
angdtech <angelinadyer13@gmail.com>
2025-08-31 11:34:16 +0200

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot@1.1.2";
-import { cva, type VariantProps } from "class-variance-authority@0.7.1";
-
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "./utils";
 
 const badgeVariants = cva(
@@ -33,7 +32,6 @@ function Badge({
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
   const Comp = asChild ? Slot : "span";
-
   return (
     <Comp
       data-slot="badge"
